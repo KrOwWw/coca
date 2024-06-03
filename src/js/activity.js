631031b327632e467cd9b1e0a94cff564794fd68
Ruslan <qwertyuiopakustalb@mail.ru>
2024-06-03 18:59:49 +0300

    Created design section

--- src/js/activity.js
+++ src/js/activity.js
@@ -2,8 +2,12 @@ import '/scss/activity.scss';
 
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { useMissionSlider } from './components/activity/slider.js';
+import {
+  useMissionSlider,
+  useArticlesSlider,
+} from './components/activity/slider.js';
 
 useTheme();
 useBurger();
 useMissionSlider();
+useArticlesSlider();
--- src/js/components/activity/slider.js
+++ src/js/components/activity/slider.js
@@ -22,3 +22,23 @@ export const useMissionSlider = () => {
     },
   });
 };
+export const useArticlesSlider = () => {
+  new Swiper('.__slider', {
+    // slidesPerView: 2.5,
+    spaceBetween: 30,
+    centeredSlides: false,
+    breakpoints: {
+      768: {
+        slidesPerView: 2.5,
+      },
+      576: {
+        spaceBetween: 15,
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 3,
+      },
+    },
+  });
+};
