694f21ebda3341817cca35536bc415f7c08a057d
Ruslan <qwertyuiopakustalb@mail.ru>
2024-07-15 18:07:30 +0300

    Added ymaps

--- src/js/components/contacts/map.js
+++ src/js/components/contacts/map.js
@@ -1,17 +1,75 @@
 export const useMap = () => {
-  initMap();
+  let waterLayer;
+
   async function initMap() {
     await ymaps3.ready;
 
-    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+    const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
+    const { YMapDefaultMarker } = await ymaps3.import(
+      '@yandex/ymaps3-markers@0.0.1',
+    ); // Импортируем YMapDefaultMarker
 
-    const map = new YMap(document.getElementById('map'), {
+    let currentTheme = localStorage.getItem('theme') || 'light';
+
+    const map = new YMap(document.getElementById('mapd'), {
       location: {
-        center: [37.588144, 55.733842],
-        zoom: 10,
+        center: [47.024643, 28.83158],
+        zoom: 2,
       },
     });
 
-    map.addChild(new YMapDefaultSchemeLayer());
+    const featuresLayer = new YMapDefaultFeaturesLayer();
+    map.addChild(featuresLayer);
+
+    waterLayer = new YMapDefaultSchemeLayer({
+      theme: 'light',
+      zIndex: 0,
+      background: {
+        stylers: [
+          {
+            opacity: 0,
+          },
+        ],
+      },
+      customization: [
+        {
+          tags: {
+            all: ['water'],
+          },
+          stylers: [
+            {
+              color: currentTheme === 'dark' ? '#1e1d25' : '#FFF',
+            },
+          ],
+        },
+        {
+          tags: {
+            all: ['landscape'],
+          },
+          stylers: [
+            {
+              color: '#acacb9',
+            },
+          ],
+        },
+      ],
+    });
+
+    map.addChild(waterLayer);
+
+    map.addChild(
+      new YMapDefaultMarker({
+        coordinates: [130, -25],
+        title: 'Yogja, INA',
+        subtitle: '100 Smith Street<br>Collingwood VIC 3066 AU',
+        color: 'blue',
+      }),
+    );
+
+    window.YMapDefaultSchemeLayer = YMapDefaultSchemeLayer;
+    window.waterLayer = waterLayer;
+    window.map = map;
   }
+
+  initMap();
 };
--- src/js/contact.js
+++ src/js/contact.js
@@ -1,11 +1,13 @@
 import '/scss/contact.scss';
 
 // components
+import { useMap } from './components/contacts/map.js';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { usePhone } from './components/contacts/phone.js';
 import { usePartnersSlider } from './components/home/slider.js';
 
+useMap();
 useTheme();
 useBurger();
 usePhone();
